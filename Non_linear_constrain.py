2ee1d1aa19565f5fa896bafca09c4bb724b4e997
Arvind Singh <arvind8198@gmail.com>
2021-09-02 14:09:25 +0530

    Update Non_linear_constrain.py

--- Non_linear_constrain.py
+++ Non_linear_constrain.py
@@ -1,3 +1,48 @@
+''' Problem:
+    Binary option trading is a type of trading in which traders have to predict the result of a yes/no situation by the end of a determined period, and
+    the payoff is either a fixed amount of money as compensation or nothing else.Trading on 1-minute Binary options, a binary trader want to minimize his 
+    loss, if the algorithms he build runs into losses. He build following two algoriths:
+    Algorith One: For the very first bet, he bets on 'UP' option, and in the next bet, he bets on 'DOWN' option. He follow the strategy of changing the bet 
+                  option on every next step.
+    Algorithm Two: In this algorithm, he follow the same strategy as in the first algorithm, but start with 'DOWN' option.
+    Let initial bet amount are x[0], x[1] in algorthm one and two respectively.In both the strategy, if he lose, he multiply the bet amount by x[3]>0., and 
+    if he wins, he multiply the bet amount by x[2]>0.
+    Study of algoriths for first two step:
+    Suppose, the trader gets 90% return on his investment, if he wins else he loses his investment.There are four possible cases.
+    case 1: 'UP' 'UP'
+           Algorithm One: Profit in first step and loss in the second step
+           Algorithm Two: Loss in the first step and profit in the second step
+           total profit = x[0]*0.9 (algo1 first step) + x[2]*0.9*x[1] (algo2 second step)
+           total loss   = x[1] (algo2 first step) + x[2] *x[0] (algo1 second step)
+           equation:
+                    total profit > total loss
+                    0.9*(x[0] + x[2]*x[1]) > x[1] + x[2]*x[0] 
+    case 2: 'DOWN' 'DOWN'
+           Algorithm One: Loss in first step and profit in the second step
+           Algorithm Two: Profit in the first step and loss in the second step
+           total profit = x[1]*0.9 (algo2 first step) + x[3]*0.9*x[0] (algo1 second step)
+           total loss   = x[0] (algo1 first step) + x[3]*x[1] (algo2 second step)
+           equation:
+                    total profit > total loss
+                    0.9*(x[1] + x[3]*x[0]) > x[0] + x[3]*x[1]
+    case 3: 'UP' 'DOWN'
+           Algorithm One: Profit in first step and profit in the second step
+           Algorithm Two: Loss in the first step and loss in the second step
+           total profit = x[0]*0.9 (algo1 first step) + x[3]*0.9*x[0] (algo1 second step)
+           total loss   = x[1] (algo2 first step) + x[2]*x[1] (algo2 second step)
+           equation:
+                    total profit > total loss
+                    0.9*(x[0] + x[3]*x[0]) > x[1] + x[2]*x[1]
+    case 4: 'DOWN' 'UP'
+           Algorithm One: Loss in first step and loss in the second step
+           Algorithm Two: Profit in the first step and profit in the second step
+           total profit = x[1]*0.9 (algo2 first step) + x[3]*0.9*x[1] (algo2 second step)
+           total loss   = x[0] (algo2 first step) + x[2]*x[0] (algo1 second step)
+           equation:
+                    profit = 0.9*(x[1] + x[3]*x[1]) , loss =  x[0] + x[2]*x[1]
+                    Objective function = 0.9*(x[1] + x[3]*x[1]) - x[0] + x[2]*x[1]
+    Third and fourth cases are similar and analysis of the problem says that the trader have to bear loss either in third or fourth case. Here, trader chose to 
+    bear loss in the fourth case.The aim of the program is to find optimum values of x[0]--x[3], so that objective function in 'case 4' is minimized'''
 import numpy as np
 from scipy.optimize import minimize
 
